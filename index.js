1a22ba2fded40decb4f04f1e648f4045dffe63a8
Troy Dorman <troydorman@gmail.com>
2021-08-17 07:41:43 -0600

    working on the pay post route. added create payment object for the transcation

--- index.js
+++ index.js
@@ -15,4 +15,37 @@ const app = express();
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("index"));
 
+app.post("/pay", (req, res) => {
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "Red Sox Hat",
+              sku: "001",
+              price: "25.00",
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          total: "25.00",
+          currency: "USD",
+        },
+        description: "MLB baseball team hat",
+      },
+    ],
+  };
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
