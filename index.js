70c850fdd881479f1f4ba3a6c691310600f15abb
Troy Dorman <troydorman@gmail.com>
2021-08-17 07:36:16 -0600

    installed dotenv and defined our paypal client id and secret ids in it

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const ejs = require("ejs");
 const paypal = require("paypal-rest-sdk");
+// require dot env
+require("dotenv").config();
+
+paypal.configure({
+  mode: "sandbox", // sandbox or live
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+});
 
 const app = express();
 
